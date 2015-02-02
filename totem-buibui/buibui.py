dbe73ee62ff3dc146e88f6e16acac8a99d111666
Sean Lin <netsnail@gmail.com>
2015-02-03 00:01:24 +0800

    开始动起来了

--- totem-buibui/buibui.py
+++ totem-buibui/buibui.py
@@ -1,95 +1,6 @@
 import time
-from gi.repository import GLib, GObject, Peas, Clutter
+from gi.repository import GObject, Peas, Gtk, GLib, GtkClutter, Clutter
 from danmaku import DanmakuStream
-
-
-class DanmukuWraper(Clutter.Group):
-    def __init__(self, dmk):
-        self.dmk = dmk
-        text = Clutter.Text()
-        text.set_color(Clutter.Color.from_string(dmk.color)[1])
-        text.set_text(dmk.text)
-        text.set_font_name(dmk.font)
-        self._drawObject = text
-        self.add_child(text)
-
-        self._shadowBR = self.build_shadow(dmk, +1, +1)
-        self._shadowTL = self.build_shadow(dmk, -1, -1)
-        self._shadowBL = self.build_shadow(dmk, -1, +1)
-        self._shadowTR = self.build_shadow(dmk, +1, -1)
-
-        self._width = dmk._drawObject.get_width() + 1
-        self._height = dmk._drawObject.get_height() + 1
-
-    def build_shadow(self, dmk, x, y):
-        shadow = Clutter.Text()
-        shadow.set_color(Clutter.Color.from_string("#000000")[1])
-        shadow.set_text(dmk.text)
-        shadow.set_font_name(dmk.font)
-        shadow.set_position(x, y)
-        self.add_child(shadow)
-
-    def tick(self):
-        if self.dmk.tick():
-            self.set_position(self.dmk.x, self.dmk.y)
-            return True
-        else:
-            return False
-
-
-class DanmakuManager(Clutter.Actor):
-    def __init__(self, screen):
-        self._last_tick = 0
-        self._screen = screen
-        self._danmukus = []
-
-        self.width = None
-        self.height = None
-
-        self.is_playing = False
-        #GLib.timeout_add(20, self.timer)
-
-    def set_stream(self, host, port):
-        self._stream = DanmakuStream(host, port)
-
-    def resume(self):
-        self.is_playing = True
-
-    def stop(self):
-        self.is_playing = False
-
-    def clear(self):
-        for dmk in self._danmuku:
-            self.remove_child(dmk)
-        self._danmuku = []
-
-    def timer(self, time):
-        if not self.is_playing:
-            return True
-        self._danmuku = [
-            i for i in self._danmuku if i.tick()
-        ]
-        return True
-
-    def tick(self, now):
-        pass
-
-    def state_change(self, *arg):
-        self.set_bounds()
-        return False
-
-    def set_bounds(self, *arg):
-        stage = self.get_stage()
-
-        s_height = stage.get_height()
-        s_width = stage.get_width()
-        self.width = s_width
-        self.height = s_height
-
-        # Set actor dimensions
-        self.set_position(0, 0)
-        self.set_size(s_width, s_height)
-        return False
 
 
 class Buibui(GObject.Object, Peas.Activatable):
@@ -99,12 +10,14 @@ class Buibui(GObject.Object, Peas.Activatable):
 
     def __init__(self):
         GObject.Object.__init__(self)
+        self._totem = None
 
     def do_activate(self):
         self._totem = self.object
 
         video = self._totem.get_video_widget()
         self._dm = DanmakuManager(self._totem)
+        self._dm.set_stream('127.0.0.1', 80)
 
         video.get_stage().add_child(self._dm)
         video.get_toplevel().connect(
@@ -126,13 +39,119 @@ class Buibui(GObject.Object, Peas.Activatable):
 
     def tick_handler(self, video, cur_time, st_length, cur_pos, user_data):
         if self._totem.is_playing():
-            if not self._cm.is_playing:
-                self._cm.resume()
+            if not self._dm.is_playing:
+                self._dm.resume()
+            self._dm.tick(cur_time)
         else:
-            self._cm.stop()
-        print cur_time, st_length, cur_pos
-        self._cm.tick(cur_time)
+            self._dm.stop()
 
     def end_handler(self, user_data):
         self._dm.stop()
         self._dm.clear()
+
+
+class DanmakuWrapper(Clutter.Group):
+    def __init__(self, dmk):
+        super(DanmakuWrapper, self).__init__()
+        self.dmk = dmk
+        text = Clutter.Text()
+        text.set_color(Clutter.Color.from_string(dmk.color)[1])
+        text.set_text(dmk.text)
+        text.set_font_name(dmk.font)
+        self._drawObject = text
+
+        self._shadowBR = self.build_shadow(dmk, +1, +1)
+        self._shadowTL = self.build_shadow(dmk, -1, -1)
+        self._shadowBL = self.build_shadow(dmk, -1, +1)
+        self._shadowTR = self.build_shadow(dmk, +1, -1)
+
+        self.add_child(text)
+
+        self._width = self._drawObject.get_width() + 1
+        self._height = self._drawObject.get_height() + 1
+
+    def build_shadow(self, dmk, x, y):
+        shadow = Clutter.Text()
+        shadow.set_color(Clutter.Color.from_string("#000000")[1])
+        shadow.set_text(dmk.text)
+        shadow.set_font_name(dmk.font)
+        shadow.set_position(x, y)
+        self.add_child(shadow)
+
+    def tick(self, now):
+        if self.dmk.tick(now):
+            self.set_position(self.dmk.x, self.dmk.y)
+            return True
+        else:
+            return False
+
+
+class DanmakuManager(Clutter.Actor):
+    def __init__(self, screen):
+        super(DanmakuManager, self).__init__()
+        self._last_tick = 0
+        self._screen = screen
+        self._danmakus = []
+
+        self.width = None
+        self.height = None
+
+        self.is_playing = False
+        self.last_time = time.time() * 1000
+
+        GLib.timeout_add(20, self.timer)
+
+    def set_stream(self, host, port):
+        self._stream = DanmakuStream(host, port)
+
+    def resume(self):
+        self.is_playing = True
+
+    def stop(self):
+        self.is_playing = False
+
+    def clear(self):
+        for dmk in self._danmakus:
+            self.remove_child(dmk)
+        self._danmakus = []
+
+    def timer(self, *args):
+        if not self.is_playing:
+            self.last_time = time.time() * 1000
+            return True
+        now = time.time() * 1000
+        duration = now - self.last_time
+        new_danmakus = []
+        for i in self._danmakus:
+            if i.tick(duration):
+                new_danmakus.append(i)
+            else:
+                self.remove_child(i)
+        self._danmakus = new_danmakus
+        self.last_time = now
+        return True
+
+    def tick(self, tick):
+        now = time.time() * 1000
+        dmks = self._stream.get_danmakus(tick)
+        dmks = [DanmakuWrapper(
+            i.setup_ctx(self.width, self.height).start(now))
+            for i in dmks
+        ]
+        [self.add_child(i) for i in dmks]
+        self._danmakus.extend(dmks)
+
+    def state_change(self, *arg):
+        self.set_bounds()
+        return False
+
+    def set_bounds(self, *arg):
+        stage = self.get_stage()
+
+        self.width = stage.get_height()
+        self.height = stage.get_width()
+
+        # Set actor dimensions
+        self.set_position(0, 0)
+        self.set_size(self.width, self.height)
+        return False
--- totem-buibui/danmaku.py
+++ totem-buibui/danmaku.py
@@ -12,32 +12,43 @@ D_MODE_BOTTOM = 3
 class Danmaku(object):
     DEFAULT_FONT = 'SimHei'
 
-    __slot__ = ['text', 'color', 'x', 'y', 'font', 'size']
+    __slot__ = ['text', 'color', 'x', 'y', 'font', 'size', 'stime', 'ctx']
 
     def __init__(self, text, color="#ffffff", size=25):
         self.text = text
+        self.color = color
         self.font_name = self.DEFAULT_FONT
         self.size = size
+        self.stime = None
+        self.x = None
+        self.y = None
 
-    def start(self, screen, width, height):
-        raise NotImplementedError
+    def start(self, now):
+        self.stime = now
+        return self
 
-    def tick(self, tick):
-        raise NotImplementedError
+    def setup_ctx(self, s_width, s_height):
+        self.ctx = {"width": s_width, "height": s_height}
+        return self
 
     def get_font_string(self):
         return "%s %d" % (self.font_name, self.size)
     font = property(get_font_string)
 
+    def tick(self, tick):
+        raise NotImplementedError
+
 
 class DanmakuRight2Left(Danmaku):
-    def setup(self, s_width, s_height, width, height):
-        self._s_width = s_width
-        self._s_height = s_height
-        self._width = width
-        self._height = height
+    def tick(self, now):
+        if self.x == None or self.y == None:
+            self.x = self.ctx['width']
+            self.y = 100
 
-    def tick(self, tick):
+        self.x -= 1
+        if self.x == 0:
+            print 'clean'
+            return False
         return True
 
 
@@ -45,6 +56,11 @@ class DanmakuStream(object):
     def __init__(self, host, port):
         self._host = host
         self._port = port
+        self._dummy_flag = False
 
-    def get_danmuku():
-        return DanmakuRight2Left("hello", start=100)
+    def get_danmakus(self, now):
+        if self._dummy_flag:
+            return []
+
+        self._dummy_flag = True
+        return [DanmakuRight2Left("hello world")]
