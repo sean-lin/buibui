fb0de79788181e862227956891e8704705162234
Sean Lin <netsnail@gmail.com>
2016-01-29 16:02:13 +0800

    加入网络处理

--- uwp_player/uwp_player/MainWindow.xaml.cs
+++ uwp_player/uwp_player/MainWindow.xaml.cs
@@ -1,22 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace uwp_player
 {
+    using LitJson;
     using System.Diagnostics;
+    using System.IO;
+    using System.Net;
     using System.Windows.Threading;    /// <summary>
                                        /// MainWindow.xaml 的交互逻辑
                                        /// </summary>
@@ -27,6 +23,8 @@ namespace uwp_player
         public const int DANMAKU_TTL = 10;
         public const int HORIZONTAL_PADDING = 20;
         public const int LAYER_OFFSET = 15;
+        public const string DANMAKU_SERVER_URL = "http://172.16.100.117:8080/buibui/get_danmakus";
+        public const int DANMAKU_SERVER_QUERY_INTERVAL = 1;
     };
 
     public enum DanmakuMode
@@ -94,14 +92,14 @@ namespace uwp_player
             base_line.ForEach((i) =>
             {
                 var y = ((TranslateTransform)i.RenderTransform).Y;
-                ret.Add(new Position(y, i.Height));
+                ret.Add(new Position(y, i.DesiredSize.Height));
             });
             ret.Add(new Position(canvas.Height, 0));
             ret.Sort(positionCmp);
             return ret;
         }
 
-        private int positionCmp(Position x, Position y)
+        protected int positionCmp(Position x, Position y)
         {
             if (x.Item1 > y.Item1)
             {
@@ -119,7 +117,7 @@ namespace uwp_player
             return pool.FindAll((i) =>
             {
                 var x = ((TranslateTransform)i.RenderTransform).X;
-                return i.Width + x + DanmakuConfig.HORIZONTAL_PADDING > canvas.Width;
+                return i.DesiredSize.Width + x + DanmakuConfig.HORIZONTAL_PADDING > canvas.Width;
             });
         }
 
@@ -132,8 +130,8 @@ namespace uwp_player
             {
                 free(tb);
             };
-            trans.BeginAnimation(TranslateTransform.XProperty, ani);
             trans.Y = y;
+            trans.BeginAnimation(TranslateTransform.XProperty, ani);
 
             tb.RenderTransform = trans;
 
@@ -181,7 +179,7 @@ namespace uwp_player
 
             TranslateTransform trans = new TranslateTransform();
             trans.Y = y;
-            trans.X = (canvas.Width - tb.Width) / 2;
+            trans.X = (canvas.Width - tb.DesiredSize.Width) / 2;
             tb.RenderTransform = trans;
 
             canvas.Children.Add(tb);
@@ -193,6 +191,43 @@ namespace uwp_player
                 timer.Stop();
             };
             timer.Start();
+        }
+    }
+    public class DanmakuBottomLayerAllocator: DanmakuTopLayerAllocator
+    {
+        public DanmakuBottomLayerAllocator(Canvas c, double offset) : base(c, offset) { }
+
+        public override bool allocate(TextBlock tb)
+        {
+            var height = tb.DesiredSize.Height;
+
+            var barrier = getBarrierPos();
+            var y = canvas.Height - height - offset;
+            foreach(var i in barrier)
+            {
+                if(i.Item1 + i.Item2 < y && i.Item1 < y)
+                {
+                    bindTranslateTransform(tb, y);
+                    return true;
+                }
+                y = i.Item1 - height - 1;
+            }
+            return false;
+        }
+        protected override List<Position> getBarrierPos()
+        {
+            var ret = new List<Position>();
+            List<TextBlock> base_line = getBarrier();
+
+            base_line.ForEach((i) =>
+            {
+                var y = ((TranslateTransform)i.RenderTransform).Y;
+                ret.Add(new Position(y, i.DesiredSize.Height));
+            });
+            ret.Add(new Position(0, 0));
+            ret.Sort(positionCmp);
+            ret.Reverse();
+            return ret;
         }
     }
 
@@ -251,7 +286,7 @@ namespace uwp_player
                 case DanmakuMode.Top:
                     return new DanmakuTopLayerAllocator(canvas, offset);
                 case DanmakuMode.Bottom:
-                    break;
+                    return new DanmakuBottomLayerAllocator(canvas, offset);
             }
             return null;
         }
@@ -263,13 +298,16 @@ namespace uwp_player
     public partial class MainWindow : Window
     {
         private Dictionary<DanmakuMode, IDanmakuAllocator> allocators;
+        private WebClient wc;
+        private long last_time;
+        private Dictionary<int, double> font_map;
 
         public MainWindow()
         {
             InitializeComponent();
             System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
             dispatcherTimer.Tick += on_update_timer;
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, DanmakuConfig.DANMAKU_SERVER_QUERY_INTERVAL);
             dispatcherTimer.Start();
             mCanvas.Width = this.Width;
             mCanvas.Height = this.Height;
@@ -278,8 +316,24 @@ namespace uwp_player
             allocators.Add(DanmakuMode.Left2Right, new DanmakuAllocator(mCanvas, DanmakuMode.Left2Right));
             allocators.Add(DanmakuMode.Right2Left, new DanmakuAllocator(mCanvas, DanmakuMode.Right2Left));
             allocators.Add(DanmakuMode.Top, new DanmakuAllocator(mCanvas, DanmakuMode.Top));
+            allocators.Add(DanmakuMode.Bottom, new DanmakuAllocator(mCanvas, DanmakuMode.Bottom));
 
-            addText("hello world", Colors.Black, 40, DanmakuMode.Left2Right);
+            wc = new WebClient();
+            wc.OpenReadCompleted += (o, a) =>
+            {
+                var stream = new StreamReader(a.Result);
+                var data = JsonMapper.ToObject<HttpReq>(stream.ReadToEnd());
+                processServerResponse(data.danmakus);
+            };
+
+            TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+            last_time = (long)t.TotalSeconds * 1000;
+
+            font_map = new Dictionary<int, double>();
+            font_map[1] = 0.03;
+            font_map[2] = 0.06;
+            font_map[3] = 0.1;
+            font_map[4] = 0.15;
         }
 
         protected override void OnKeyDown(KeyEventArgs e)
@@ -291,7 +345,7 @@ namespace uwp_player
             }
         }
 
-        private void addText(String str, Color color, int size, DanmakuMode mode)
+        private void addDanmaku(String str, Color color, double size, DanmakuMode mode)
         {
             var tb = new TextBlock()
             {
@@ -308,7 +362,54 @@ namespace uwp_player
 
         private void on_update_timer(object sender, EventArgs e)
         {
-            // TODO update danmakus
+            if (!wc.IsBusy)
+            {
+                var uri = new Uri(DanmakuConfig.DANMAKU_SERVER_URL + "?ts=" + last_time.ToString());
+                wc.OpenReadAsync(uri);
+            }
         }
+
+        private void processServerResponse(List<DanmakuMessage> msgs)
+        {
+            msgs.Sort((x, y) =>
+            {
+                if (x.ts == y.ts) return 0;
+                return (x.ts > y.ts)?1:-1;
+            });
+            if(msgs.Count > 0)
+            {
+                var ts = msgs.Last().ts;
+                if(ts > last_time)
+                {
+                    last_time = ts;
+                }
+            }
+            foreach(var i in msgs)
+            {
+                var clr = (Color)ColorConverter.ConvertFromString(i.color);
+                var mode = (DanmakuMode)i.mode;
+                var size = font_map[i.size] * mCanvas.Height;
+                addDanmaku(i.text, clr, size, mode);
+            }
+        }
+
+        private long now()
+        {
+            TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+            return (long)t.TotalSeconds * 1000;
+        }
+    }
+    public struct DanmakuMessage
+    {
+        public string text;
+        public int mode;
+        public int size;
+        public string color;
+        public long ts;
+    };
+
+    public struct HttpReq
+    {
+        public List<DanmakuMessage> danmakus;
     }
 }
